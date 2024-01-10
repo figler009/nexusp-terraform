b95d65dd19f50ca24d9082fc99008461566bc064
luisfigler <luis-figler@outlook.com>
2024-01-10 16:03:23 -0700

    adding backend to tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,5 +1,7 @@
-# terraform {
-#   backend "s3" {
-
-#   }
-# }
+terraform {
+  backend "s3" {
+    key    = "terraform/terraform.tfstate"
+    bucket = "pyhon-api-tf-state"
+    region = "us-east-1"
+  }
+}
