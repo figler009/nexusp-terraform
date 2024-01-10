6e9a51121cff003e02909869ca1f70e4b9969baa
luisfigler <luis-figler@outlook.com>
2024-01-10 16:38:19 -0700

    fixing templating script finally 1

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -6,7 +6,7 @@ yes | sudo apt install python3 python3-pip
 git clone https://github.com/figler009/nexusp-api.git
 sleep 20
 # shellcheck disable=SC2164
-cd nexusp-api
+cd ./nexusp-api
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
